0aabe5e701178881b72508233e6159bb0dd796dc
WasiqB <wasbhamla2005@gmail.com>
2022-03-09 19:59:22 +0530

    fix(Test): Fixed failing tests

--- src/test/java/com/github/wasiqb/boyka/testng/web/pages/HomePage.java
+++ src/test/java/com/github/wasiqb/boyka/testng/web/pages/HomePage.java
@@ -18,6 +18,7 @@ package com.github.wasiqb.boyka.testng.web.pages;
 
 import static com.github.wasiqb.boyka.builders.Locator.buildLocator;
 import static java.text.MessageFormat.format;
+import static org.openqa.selenium.By.cssSelector;
 import static org.openqa.selenium.By.id;
 import static org.openqa.selenium.By.xpath;
 
@@ -41,7 +42,23 @@ public class HomePage {
         return new HomePage ();
     }
 
-    private final Locator menuButton = buildLocator ().web (id ("react-burger-menu-btn"))
+    private final Locator menuButton         = buildLocator ().web (id ("react-burger-menu-btn"))
+        .build ();
+    private final Locator productParent      = buildLocator ().web (cssSelector ("div.inventory_item"))
+        .build ();
+    private final Locator productDescription = buildLocator ().parent (this.productParent)
+        .web (cssSelector ("div.inventory_item_desc"))
+        .build ();
+    private final Locator addToCartButton    = buildLocator ().parent (this.productParent)
+        .web (id ("add-to-cart-sauce-labs-backpack"))
+        .build ();
+    private final Locator productPrice       = buildLocator ().parent (this.productParent)
+        .web (cssSelector ("div.inventory_item_price"))
+        .build ();
+    private final Locator productTitle       = buildLocator ().parent (this.productParent)
+        .web (cssSelector ("div.inventory_item_name"))
+        .build ();
+    private final Locator shoppingCartCount  = buildLocator ().web (cssSelector ("span.shopping_cart_badge"))
         .build ();
 
     private HomePage () {
