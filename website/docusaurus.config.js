60bd873b0b0228571bfba0e42788dcea1ec1502e
WasiqB <wasbhamla2005@gmail.com>
2022-05-24 09:27:49 +0530

    fix(website): :lipstick: fixed styling issue on Mobile

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: true,
       },
       tableOfContents: {
