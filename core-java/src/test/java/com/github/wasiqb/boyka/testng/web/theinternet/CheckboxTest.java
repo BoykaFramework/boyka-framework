a34a1260738ae2bdd646237c9acfde7959072a92
Wasiq Bhamla <wasbhamla2005@gmail.com>
2022-08-05 17:21:21 +0530

    fix(java): :white_check_mark: added tests for few mouse actions (#137)
    * test(java): :white_check_mark: added tests for few mouse actions
    
    also updated workflow to run on develop branch instead of main
    
    * test(java): :white_check_mark: added checkbox tests

--- core-java/src/test/java/com/github/wasiqb/boyka/testng/web/theinternet/CheckboxTest.java
+++ core-java/src/test/java/com/github/wasiqb/boyka/testng/web/theinternet/CheckboxTest.java
@@ -17,16 +17,11 @@
 package com.github.wasiqb.boyka.testng.web.theinternet;
 
 import static com.github.wasiqb.boyka.actions.DriverActions.navigateTo;
-import static com.github.wasiqb.boyka.actions.MouseActions.clickAndHold;
 import static com.github.wasiqb.boyka.actions.MouseActions.clickOn;
-import static com.github.wasiqb.boyka.actions.MouseActions.doubleClickOn;
-import static com.github.wasiqb.boyka.actions.MouseActions.hoverOn;
-import static com.github.wasiqb.boyka.actions.VerifyDriverActions.verifyAcceptAlert;
-import static com.github.wasiqb.boyka.actions.VerifyElementActions.verifyStyleOf;
-import static com.github.wasiqb.boyka.actions.VerifyElementActions.verifyTextOf;
+import static com.github.wasiqb.boyka.actions.VerifyElementActions.verifyElementSelected;
 import static com.github.wasiqb.boyka.manager.DriverManager.closeDriver;
 import static com.github.wasiqb.boyka.manager.DriverManager.createDriver;
-import static com.github.wasiqb.boyka.testng.web.theinternet.pages.DoubleClickPage.doubleClickPage;
+import static com.github.wasiqb.boyka.testng.web.theinternet.pages.CheckboxPage.checkboxPage;
 
 import com.github.wasiqb.boyka.enums.ApplicationType;
 import org.testng.annotations.AfterClass;
@@ -34,14 +29,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-/**
- * Drag and drop test.
- *
- * @author Wasiq Bhamla
- * @since 26-Jul-2022
- */
-public class DoubleClickTest {
-    private static final String URL = "https://webdriveruniversity.com/Actions/index.html";
+public class CheckboxTest {
+    private static final String URL = "https://the-internet.herokuapp.com/checkboxes";
 
     /**
      * Setup test class by initialising driver.
@@ -65,30 +54,13 @@ public class DoubleClickTest {
     }
 
     /**
-     * Test click and hold method.
+     * Test select checkbox.
      */
-    @Test (description = "Verify Click and Hold method")
-    public void testClickAndHold () {
-        clickAndHold (doubleClickPage ().getClickHold ());
-        verifyTextOf (doubleClickPage ().getClickHold ()).isEqualTo ("Well done! keep holding that click now.....");
-    }
-
-    /**
-     * Double click test.
-     */
-    @Test (description = "Double click test")
-    public void testDoubleClick () {
-        doubleClickOn (doubleClickPage ().getDoubleClick ());
-        verifyStyleOf (doubleClickPage ().getDoubleClick (), "background-color").isEqualTo ("rgba(147, 203, 90, 1)");
-    }
-
-    /**
-     * Test hover and click method.
-     */
-    @Test (description = "Verify Click and Hold method")
-    public void testHoverAndClick () {
-        hoverOn (doubleClickPage ().getHoverButton ());
-        clickOn (doubleClickPage ().getHoverMenu ());
-        verifyAcceptAlert ().isEqualTo ("Well done you clicked on the link!");
+    @Test (description = "Verify select checkbox")
+    public void testCheckOption () {
+        verifyElementSelected (checkboxPage ().getOption1 ()).isFalse ();
+        verifyElementSelected (checkboxPage ().getOption2 ()).isTrue ();
+        clickOn (checkboxPage ().getOption1 ());
+        verifyElementSelected (checkboxPage ().getOption1 ()).isTrue ();
     }
 }
