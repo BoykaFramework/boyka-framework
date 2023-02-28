8fc83e781d4d265247c72179121f495f1138d2a1
WasiqB <wasbhamla2005@gmail.com>
2023-02-28 17:25:12 +0530

    fix(java): :bug: fixed error encountered in API

---
+++ core-java/src/test/java/com/github/wasiqb/boyka/testng/api/requests/BookingRequest.java
@@ -0,0 +1,101 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023, Wasiq Bhamla
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.github.wasiqb.boyka.testng.api.requests;
+
+import static com.github.wasiqb.boyka.actions.api.ApiActions.withRequest;
+import static com.github.wasiqb.boyka.builders.ApiRequest.createRequest;
+import static java.text.MessageFormat.format;
+
+import com.github.wasiqb.boyka.builders.ApiRequest;
+import com.github.wasiqb.boyka.enums.RequestMethod;
+
+/**
+ * Booking request class
+ *
+ * @author Wasiq Bhamla
+ * @since 28-Feb-2023
+ */
+public class BookingRequest {
+    private static final String API_CONFIG_KEY = "test_restfulbooker";
+
+    public static ApiRequest createBooking (final Object requestBody) {
+        return createRequest ().configKey (API_CONFIG_KEY)
+            .method (RequestMethod.POST)
+            .header ("Accept", "application/json")
+            .path ("/booking")
+            .bodyObject (requestBody)
+            .create ();
+    }
+
+    public static ApiRequest deleteBooking (final String id) {
+        return createRequest ().configKey (API_CONFIG_KEY)
+            .method (RequestMethod.DELETE)
+            .header ("Content-Type", "application/json")
+            .header ("Cookie", format ("token={0}", generateToken ()))
+            .path ("/booking/${id}")
+            .pathParam ("id", id)
+            .create ();
+    }
+
+    public static ApiRequest getBooking (final String id) {
+        return createRequest ().configKey (API_CONFIG_KEY)
+            .method (RequestMethod.GET)
+            .header ("Accept", "application/json")
+            .path ("/booking/${id}")
+            .pathParam ("id", id)
+            .create ();
+    }
+
+    public static ApiRequest updateBooking (final String id, final Object requestBody) {
+        return createRequest ().configKey (API_CONFIG_KEY)
+            .method (RequestMethod.PUT)
+            .header ("Accept", "application/json")
+            .header ("Cookie", format ("token={0}", generateToken ()))
+            .path ("/booking/${id}")
+            .bodyObject (requestBody)
+            .pathParam ("id", id)
+            .create ();
+    }
+
+    public static ApiRequest updatePartialBooking (final String id, final Object requestBody) {
+        return createRequest ().configKey (API_CONFIG_KEY)
+            .method (RequestMethod.PATCH)
+            .header ("Accept", "application/json")
+            .header ("Cookie", format ("token={0}", generateToken ()))
+            .path ("/booking/${id}")
+            .bodyObject (requestBody)
+            .pathParam ("id", id)
+            .create ();
+    }
+
+    private static String generateToken () {
+        final TokenBuilder builder = new TokenBuilder ();
+        final var generateTokenRequest = createRequest ().configKey (API_CONFIG_KEY)
+            .header ("Accept", "application/json")
+            .method (RequestMethod.POST)
+            .path ("/auth")
+            .bodyObject (builder.tokenBuilder ())
+            .create ();
+
+        final var response = withRequest (generateTokenRequest).execute ();
+        return response.getResponseData ("token");
+    }
+
+    private BookingRequest () {
+        // Utility class.
+    }
+}
