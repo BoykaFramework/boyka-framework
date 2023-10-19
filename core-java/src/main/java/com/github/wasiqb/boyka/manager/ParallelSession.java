923e8f10fc3196525a0d01fa6352255fae69d696
Wasiq Bhamla <wasbhamla2005@gmail.com>
2023-10-19 18:12:13 +0530

    refactor(java): :recycle: refactored getSession method logic (#635)

--- core-java/src/main/java/com/github/wasiqb/boyka/manager/ParallelSession.java
+++ core-java/src/main/java/com/github/wasiqb/boyka/manager/ParallelSession.java
@@ -16,14 +16,17 @@
 
 package com.github.wasiqb.boyka.manager;
 
+import static com.github.wasiqb.boyka.enums.Message.SESSION_ALREADY_CLEARED;
+import static com.github.wasiqb.boyka.enums.Message.SESSION_ALREADY_CREATED;
+import static com.github.wasiqb.boyka.enums.Message.SESSION_NOT_CREATED;
 import static com.github.wasiqb.boyka.enums.Message.SESSION_PERSONA_CANNOT_BE_NULL;
 import static com.github.wasiqb.boyka.enums.PlatformType.API;
 import static com.github.wasiqb.boyka.enums.PlatformType.WEB;
+import static com.github.wasiqb.boyka.utils.ErrorHandler.throwError;
 import static com.github.wasiqb.boyka.utils.Validator.requireNonEmpty;
 import static java.lang.ThreadLocal.withInitial;
 import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.apache.logging.log4j.LogManager.getLogger;
 
 import java.util.HashMap;
@@ -49,16 +52,16 @@ public final class ParallelSession {
     /**
      * Clears all the sessions.
      */
-    public static void clearAllSessions () {
+    public static synchronized void clearAllSessions () {
         LOGGER.info ("Clearing all the sessions...");
         final var sessions = SESSION.get ();
-        sessions.forEach ((persona, session) -> {
-            if (isNotEmpty (persona)) {
-                switchPersona (persona);
-                clearSession ();
-            }
-        });
-        sessions.clear ();
+        final var sessionList = sessions.keySet ()
+            .stream ()
+            .toList ();
+        for (final var persona : sessionList) {
+            switchPersona (persona);
+            clearSession ();
+        }
         SESSION.remove ();
     }
 
@@ -67,23 +70,20 @@ public final class ParallelSession {
      */
     public static void clearSession () {
         LOGGER.info ("Clearing session for persona [{}]...", getCurrentPersona ());
+        final var session = SESSION.get ();
+        if (!isSessionCreated ()) {
+            throwError (SESSION_ALREADY_CLEARED);
+        }
         getSession ().clearListeners ();
         getSession ().clearSharedData ();
         ofNullable (getSession ().getDriver ()).ifPresent (WebDriver::quit);
         if (getSession ().getPlatformType () != WEB) {
             ofNullable (getSession ().getServiceManager ()).ifPresent (ServiceManager::stopServer);
         }
+        if (isSessionCreated ()) {
+            session.remove (getCurrentPersona ());
+        }
         CURRENT_PERSONA.remove ();
-    }
-
-    /**
-     * Create a new Session.
-     *
-     * @param platformType Target Platform Type
-     * @param configKey Configuration key for the session
-     */
-    public static void createSession (final PlatformType platformType, final String configKey) {
-        createSession (configKey, platformType, configKey);
     }
 
     /**
@@ -96,13 +96,24 @@ public final class ParallelSession {
     public static synchronized void createSession (final String persona, final PlatformType platformType,
         final String configKey) {
         switchPersona (persona);
-        final var currentSession = getSession ();
+        final var currentSession = new DriverSession<> ();
         currentSession.setPlatformType (platformType);
         currentSession.setConfigKey (configKey);
+        setSession (currentSession);
         if (platformType != API) {
             final var instance = new DriverManager ();
             instance.setupDriver ();
         }
+    }
+
+    /**
+     * Create a new Session.
+     *
+     * @param platformType Target Platform Type
+     * @param configKey Configuration key for the session
+     */
+    public static void createSession (final PlatformType platformType, final String configKey) {
+        createSession (configKey, platformType, configKey);
     }
 
     /**
@@ -125,14 +136,21 @@ public final class ParallelSession {
     public static synchronized <D extends WebDriver> DriverSession<D> getSession () {
         LOGGER.traceEntry ();
         final var currentPersona = getCurrentPersona ();
-        final var session = SESSION.get ()
-            .containsKey (currentPersona);
-        if (!session) {
-            SESSION.get ()
-                .put (currentPersona, new DriverSession<> ());
+        if (!isSessionCreated ()) {
+            throwError (SESSION_NOT_CREATED);
         }
         return LOGGER.traceExit ((DriverSession<D>) SESSION.get ()
             .get (currentPersona));
+    }
+
+    /**
+     * Checks if the session has been started.
+     *
+     * @return true, if session has been created, else, false.
+     */
+    public static boolean isSessionCreated () {
+        return SESSION.get ()
+            .containsKey (getCurrentPersona ());
     }
 
     /**
@@ -161,6 +179,16 @@ public final class ParallelSession {
         LOGGER.traceEntry ("Application Type: {}, Driver: {}", session.getPlatformType (), driver);
         session.setDriver (driver);
         LOGGER.traceExit ();
+    }
+
+    private static synchronized <D extends WebDriver> void setSession (final DriverSession<D> session) {
+        final var persona = getCurrentPersona ();
+        final var currentSession = SESSION.get ();
+        if (!isSessionCreated ()) {
+            currentSession.put (persona, session);
+        } else {
+            throwError (SESSION_ALREADY_CREATED, persona);
+        }
     }
 
     private ParallelSession () {
