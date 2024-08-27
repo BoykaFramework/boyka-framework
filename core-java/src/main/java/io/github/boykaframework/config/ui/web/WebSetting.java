945c33fd1e3cd01dbb65d1de0dcae46a9b1151a1
WasiqB <wasbhamla2005@gmail.com>
2024-08-27 15:53:39 +0300

    fix(java): :bug: fixed failing web cloud tests

--- core-java/src/main/java/io/github/boykaframework/config/ui/web/WebSetting.java
+++ core-java/src/main/java/io/github/boykaframework/config/ui/web/WebSetting.java
@@ -19,6 +19,7 @@ package io.github.boykaframework.config.ui.web;
 import static io.github.boykaframework.enums.Protocol.HTTP;
 import static io.github.boykaframework.enums.TargetProviders.LOCAL;
 import static io.github.boykaframework.enums.WindowResizeType.NORMAL;
+import static io.github.boykaframework.utils.StringUtils.interpolate;
 
 import java.util.List;
 import java.util.Map;
@@ -57,4 +58,22 @@ public class WebSetting implements BoykaConfig {
     private TargetProviders     target           = LOCAL;
     private String              userName;
     private String              version          = "stable";
+
+    /**
+     * Gets cloud password.
+     *
+     * @return the cloud password
+     */
+    public String getPassword () {
+        return interpolate (this.password);
+    }
+
+    /**
+     * Gets cloud user name.
+     *
+     * @return the cloud username.
+     */
+    public String getUserName () {
+        return interpolate (this.userName);
+    }
 }
