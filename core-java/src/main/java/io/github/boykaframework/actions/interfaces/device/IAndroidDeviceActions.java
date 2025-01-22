3295f2508691477ee89dd035c89a1fdd27363c6f
WasiqB <wasbhamla2005@gmail.com>
2025-01-22 11:04:55 +0300

    feat(java): :sparkles: added android specific critical actions

--- core-java/src/main/java/io/github/boykaframework/actions/interfaces/device/IAndroidDeviceActions.java
+++ core-java/src/main/java/io/github/boykaframework/actions/interfaces/device/IAndroidDeviceActions.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024, Wasiq Bhamla
+ * Copyright (c) 2024, Boyka Framework
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -16,6 +16,9 @@
 
 package io.github.boykaframework.actions.interfaces.device;
 
+import java.io.File;
+
+import com.google.common.truth.StringSubject;
 import io.appium.java_client.android.nativekey.AndroidKey;
 
 /**
@@ -26,9 +29,54 @@ import io.appium.java_client.android.nativekey.AndroidKey;
  */
 public interface IAndroidDeviceActions extends IDeviceActions {
     /**
+     * Gets the current clipboard text.
+     *
+     * @return Text in the clipboard.
+     */
+    String getClipboardText ();
+
+    /**
+     * Open notification panel.
+     */
+    void openNotification ();
+
+    /**
      * Press Android key on the device.
      *
      * @param key Android key
      */
     void pressKey (AndroidKey key);
+
+    /**
+     * Gets the file from the device.
+     *
+     * @param targetPath File path on the remote device.
+     *
+     * @return Base 64 encoded file content.
+     */
+    byte[] pullFile (String targetPath);
+
+    /**
+     * Puts file from machine to device.
+     *
+     * @param sourceFile File to put on device.
+     * @param destinationPath Remote device path where file should be put.
+     *
+     * @return true, if file successfully put on the device, else false.
+     */
+    boolean putFile (File sourceFile, String destinationPath);
+
+    /**
+     * Sets the clipboard text on the device
+     *
+     * @param text Text to set in the clipboard.
+     */
+    void setClipboardText (String text);
+
+    /**
+     * Verifies clipboard text on the device.
+     *
+     * @return String subject.
+     */
+    StringSubject verifyClipboardText ();
 }
